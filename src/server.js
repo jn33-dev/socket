687ffe916bcfbcc3f2c167e974cbf39370b77bee
yena choi <geniusyena332@gmail.com>
2022-12-23 22:00:16 +0900

    DOCS: io project setting

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
+const SocketIo = require("socket.io");
 const express = require("express");
-const ws = require("ws");
 
 const app = express();
 
@@ -11,33 +11,37 @@ app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-const server = http.createServer(app);
-const wss = new ws.Server({ server });
+const httpServer = http.createServer(app);
+const io = SocketIo(httpServer);
 
-const sockets = [];
+io.on("connection", (socket) => {
+  console.log(socket);
+});
+
+// const sockets = [];
 
 //socket 메소드 사용해서 브라우저로 데이터 송출
 // back에서의 socket은 방금 접속한 브라우저를 의미
-wss.on("connection", (socket) => {
-  sockets.push(socket);
-  socket["nickname"] = "Anon";
-  console.log("Connected to Browser ❤");
-  socket.on("close", () => {
-    sockets.splice(sockets.indexOf(socket), 1);
-    console.log("Disconnected from the Browser 💔");
-    console.log(sockets.length);
-  });
-  socket.on("message", (msg) => {
-    const message = JSON.parse(msg);
-    switch (message.type) {
-      case "message":
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${message.payload}`)
-        );
-      case "nickname":
-        socket["nickname"] = message.payload;
-    }
-  });
-});
+// wss.on("connection", (socket) => {
+//   sockets.push(socket);
+//   socket["nickname"] = "Anon";
+//   console.log("Connected to Browser ❤");
+//   socket.on("close", () => {
+//     sockets.splice(sockets.indexOf(socket), 1);
+//     console.log("Disconnected from the Browser 💔");
+//     console.log(sockets.length);
+//   });
+//   socket.on("message", (msg) => {
+//     const message = JSON.parse(msg);
+//     switch (message.type) {
+//       case "message":
+//         sockets.forEach((aSocket) =>
+//           aSocket.send(`${socket.nickname} : ${message.payload}`)
+//         );
+//       case "nickname":
+//         socket["nickname"] = message.payload;
+//     }
+//   });
+// });
 
-server.listen(3000, handleListen);
+httpServer.listen(3000, handleListen);
